38287197413f262038ebf5d9c3195fb2e2ed2ce3
lesnichiy <lesnichiy02@ya.ru>
2021-01-27 01:14:14 +0300

    feat: add to checkWinner function - check by diagonals

--- src/utils/checkWinner.js
+++ src/utils/checkWinner.js
@@ -5,44 +5,100 @@ export const checkWinner = () => {
   const { col, row, discColor } = state.gameBoard.lastDiscDropCell;
   const currentBoardArray = state.gameBoard.board;
 
-  let countSameCell = 1;
-  //horizontal check
-  // go to right
-  for (let i = col + 1; i <= 6; i++) {
-    if (currentBoardArray[i][row] === undefined) {
-      break;
-    } else if (currentBoardArray[i][row] === discColor) {
-      countSameCell++;
+  const checkHorizontal = () => {
+    let countSameCell = 1;
+    //horizontal check
+    // go right
+    for (let i = col + 1; i <= 6; i++) {
+      if (currentBoardArray[i][row] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
     }
-  }
-  //go to left
-  for (let i = col - 1; i >= 0; i--) {
-    if (currentBoardArray[i][row] === undefined) {
-      break;
-    } else if (currentBoardArray[i][row] === discColor) {
-      countSameCell++;
+    //go left
+    for (let i = col - 1; i >= 0; i--) {
+      if (currentBoardArray[i][row] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
     }
-  }
-  //vertical check
-  // go to bottom
-  for (let i = row - 1; i >= 0; i--) {
-    if (currentBoardArray[col][i] === undefined) {
-      break;
-    } else if (currentBoardArray[col][i] === discColor) {
-      countSameCell++;
-    }
-  }
-  //go to up
-  for (let i = row + 1; i <= 5; i++) {
-    if (currentBoardArray[col][i] === undefined) {
-      break;
-    } else if (currentBoardArray[col][i] === discColor) {
-      countSameCell++;
-    }
-  }
-  if (countSameCell >= 4) {
-    console.log(discColor, ' is WIN ', countSameCell);
-  }
+    return (countSameCell >= 4);
+  };
 
-  console.log(col, row, discColor, currentBoardArray);
+  const checkVertical = () => {
+    let countSameCell = 1;
+    //vertical check
+    // go to bottom
+    for (let i = row - 1; i >= 0; i--) {
+      if (currentBoardArray[col][i] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
+    }
+    //go to up
+    for (let i = row + 1; i <= 5; i++) {
+      if (currentBoardArray[col][i] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
+    }
+    return (countSameCell >= 4);
+  };
+
+  const checkPrimaryDiagonal = () => {
+    let countSameCell = 1;
+    //Primary diagonal check
+    // go down
+    for (let i = col + 1, j = row - 1; (i <= 6 && j >= 0); i++, j--) {
+      if (currentBoardArray[i][j] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
+    }
+    //go up
+    for (let i = col - 1, j = row + 1; (i >= 0 && j <= 5); i--, j++) {
+      if (currentBoardArray[i][j] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
+    }
+    return (countSameCell >= 4);
+  };
+
+  const checkSecondaryDiagonal = () => {
+    let countSameCell = 1;
+    //Secondary diagonal check
+    // go down
+    for (let i = col - 1, j = row - 1; (i >= 0 && j >= 0); i--, j--) {
+      if (currentBoardArray[i][j] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
+    }
+    //go up
+    for (let i = col + 1, j = row + 1; (i <= 6 && j <= 5); i++, j++) {
+      if (currentBoardArray[i][j] !== discColor) {
+        break;
+      } else {
+        countSameCell++;
+      }
+    }
+    return (countSameCell >= 4);
+  };
+
+  if (checkHorizontal()
+      || checkVertical()
+      || checkPrimaryDiagonal()
+      || checkSecondaryDiagonal()
+  )
+  {
+    console.log(discColor, ' is WIN ');
+  }
 };
