ee8b184c2f3ba0b9b2c80ae9582cfdaac263c0d0
lesnichiy <lesnichiy02@ya.ru>
2021-01-31 17:40:08 +0300

    feat: add stop Timer when game is over

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './Timer.module.css';
 import { useState, useEffect } from 'react';
+import store from '../../store/store';
 
 function Timer(props) {
+
+  const state = store.getState();
+  const { isGameOver } = state.appNavigation;
 
   const [ time, setTime ] = useState(0);
 
   useEffect(() => {
+    if (isGameOver) return;
     const timerID = setInterval(
         () => setTime(time + 1),
         1000
