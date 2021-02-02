5ec2ba4cc3460f7c1640be5a61209c25ccd68555
lesnichiy <lesnichiy02@ya.ru>
2021-02-03 02:32:37 +0300

    feat: add css styles for WinnerPopup

--- src/components/WinnerPopup/WinnerPopup.js
+++ src/components/WinnerPopup/WinnerPopup.js
@@ -10,9 +10,14 @@ const WinnerPopup = () => {
   return (
       <div className={styles.popupWrapper}>
         <div className={styles.popupWindow}>
-          <div>Player {color} WINS</div>
-          <div>Moves: {moves}</div>
-          <div>Time: {time}</div>
+          <div className={styles.player}>
+            <div className={styles.title}>Player</div>
+            <div className={[styles.chip, styles[color]].join(' ')}></div>
+            <div className={styles.wins}>WINS!</div>
+          </div>
+          <div className={styles.moves}>Moves: {moves}</div>
+          <div className={styles.time}>Time: {time}</div>
+          <div className={styles.offerToRequest}>You won in {moves} moves. Want to know an interesting fact about this number?</div>
         </div>
         <div className={styles.popupOverlay}></div>
       </div>
